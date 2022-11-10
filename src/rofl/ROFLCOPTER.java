e212ab4c412b9a269b4c2754bb161e26dc00c037
Caleb-Hurst <hursty.caleb@gmail.com>
2022-11-10 13:22:15 -0600

    Refactored code

--- src/rofl/ROFLCOPTER.java
+++ src/rofl/ROFLCOPTER.java
@@ -1,55 +1,58 @@
 package rofl;
 
-public class ROFLCOPTER {
+public class RoflCopter { //copter
+	//option cmd r rename for any object
 
 	public static void main(String[] args) throws InterruptedException {
+		new RoflCopter().fly();
+//		ROFLCOPTER copter = new ROFLCOPTER();
+//		copter.fly();
+		
+	}
+
+	private void fly() throws InterruptedException {
+		int c = 100;
 		while (true) {
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
+			for(int i = 0; i<10; i++) {
+				System.out.println();
+			}
 
-			System.out.println("          LOL:ROFL:ROFL                    ");
-			System.out.println("          A          ");
-			System.out.println("  L   /--------                 ");
-			System.out.println("LOL======      []\\                 ");
-			System.out.println("      \\          \\       ");
-			System.out.println("       \\_________]            ");
-			System.out.println("          I     I    ");
-			System.out.println("        ------------/            ");
-			System.out.println("                    ");
-			System.out.println("     Calebs ROFLCOPTER            ");
+			bladeRight();
 
-			Thread.sleep(100);
+			Thread.sleep(c);
 
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
+			for(int i = 0; i<10; i++) {
+				System.out.println();
+			}
 
-			System.out.println("ROFL:ROFL:LOL                  ");
-			System.out.println("          A          ");
-			System.out.println("      /--------                 ");
-			System.out.println("LOL======      []\\                  ");
-			System.out.println("  L   \\          \\       ");
-			System.out.println("       \\_________]            ");
-			System.out.println("          I     I    ");
-			System.out.println("        ------------/            ");
-			System.out.println("                    ");
-			System.out.println("     Calebs ROFLCOPTER            ");
-			Thread.sleep(100);
+			bladeLeft();
+			Thread.sleep(c);
 		}
+	}
+
+	private void bladeLeft() {
+		System.out.println("ROFL:ROFL:LOL                  ");
+		System.out.println("          A          ");
+		System.out.println("      /--------                 ");
+		System.out.println("LOL======      []\\                  ");
+		System.out.println("  L   \\          \\       ");
+		System.out.println("       \\_________]            ");
+		System.out.println("          I     I    ");
+		System.out.println("        ------------/            ");
+		System.out.println("                    ");
+		System.out.println("     Calebs ROFLCOPTER            ");
+	}
+
+	private  void bladeRight() {
+		System.out.println("          LOL:ROFL:ROFL                    ");
+		System.out.println("          A          ");
+		System.out.println("  L   /--------                 ");
+		System.out.println("LOL======      []\\                 ");
+		System.out.println("      \\          \\       ");
+		System.out.println("       \\_________]            ");
+		System.out.println("          I     I    ");
+		System.out.println("        ------------/            ");
+		System.out.println("                    ");
+		System.out.println("     Calebs ROFLCOPTER            ");
 	}
 }
